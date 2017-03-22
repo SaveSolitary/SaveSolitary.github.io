2c97d83ba5a96ab7cdc815b8f8f8b0548ad42734
SaveSolitary <savesolitarysaveval@gmail.com>
2017-03-21 19:35:45 -0700

    Add files via upload

---
+++ CODE.php
@@ -0,0 +1,22 @@
+<?php 
+
+session_start(); // must go right at the top of the page - to track login
+
+$logins = array('myUsername' => 'myPassword'); // your username and password
+
+if($_POST['Submit'] == 'Submit') { // check for form submit
+    $user = $_POST['user']; 
+    $pass = $_POST['pass']; 
+    if (isset($logins[$user]) && ($logins[$user] == $pass)) { // check login and compare credentials
+        $_SESSION['username'] = $user; // set the session
+        header('Location: another-page.php'); // redirect to protected page
+        } 
+    }
+?>
+<html>
+	<form name="loginform" method="post" action="CODE.php">
+	Username :<input type="text" name="user" /><br />
+	Password :<input type="text" name="pass" /><br />
+	<input type="submit" name="Submit" value="Submit" />
+	</form>
+</html>
