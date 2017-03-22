7f5f523ff88d109de116c8386215bfab83c45cba
SaveSolitary <savesolitarysaveval@gmail.com>
2017-03-21 19:50:48 -0700

    Add files via upload

---
+++ another-page.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start(); // must go right at the top of the page - to track login
+ 
+if($_SESSION['username']) { // check that session 
+    if($_POST['Logout'] == 'Logout') { // check for form submit
+        session_destroy(); // destroy the session to logout
+        header('Location: admin-login.php'); // redirect back to login page
+    }
+?>
+
+<!-- START - put all your content to be protected here -->
+Another page.
+<br/><br/>
+<form name="logout" method="post" action="another-page.php">
+<input type="submit" name="Logout" value="Logout" />
+</form>
+<!-- END - put all your content to be protected here -->
+
+<?php } else {
+echo "Access Denied";
+header('Location: admin-login.php'); // redirect back to login page
+}
+?>
